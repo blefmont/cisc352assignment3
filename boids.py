4eb21b8fcb09b881705ffde9ce167ffbdf48a60c
Michael Olson <michael.olson@queensu.ca>
2019-03-28 18:16:47 -0400

    more stuff to file

--- boids.py
+++ boids.py
@@ -1,1 +1,28 @@
 ### Boids rules
+class boid():
+    def __init__(self):
+        self.position = (0,0)
+        self.velocity = (0,0)
+
+def move_all_boids_to_new_positions(boids):
+    pass
+def rule1():
+    pass
+def rule2():
+    pass
+def rule3():
+    pass
+def rule4():
+    pass
+
+
+def main():
+    boids = init(5)
+    move_all_boids_to_new_positions(boids)
+
+## returns list of x boids
+def init(x):
+    pass
+
+main()
+
