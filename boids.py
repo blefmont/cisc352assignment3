8cac15b9ab9a1e23143a96389b9639f6b59cc17b
xXLinuxGodXx <connorway97@gmail.com>
2019-03-29 11:45:15 -0400

    created basic init for main function
    Also a new instantiator for boid() wherein you can give it a specified start location

--- boids.py
+++ boids.py
@@ -3,6 +3,10 @@ class boid():
     def __init__(self):
         self.position = (0,0)
         self.velocity = (0,0)
+    #creates boid at target location, with velocity (0,1)
+    def __init__(self, x, y):
+        self.position = (x,y)
+        self.velocity = (0,1)
 
 def move_all_boids_to_new_positions(boids):
     pass
@@ -22,7 +26,12 @@ def main():
 
 ## returns list of x boids
 def init(x):
-    pass
+    blist = []
+    #starts each boid 10 ositions apart
+    for i in range(0,x):
+        newBoid = boid(i*10,0)
+        bList.append(newBoid)
+    return bList
 
 main()
 
